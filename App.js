d6fa4165a79231b09f3e6c42a3c6d750f2d8ae8b
christianArby <christian.arby@gmail.com>
2020-04-17 18:11:10 +0200

    Functional todo app, looks awful though :) Local temporary data, React native state managed.

--- App.js
+++ App.js
@@ -1,36 +1,143 @@
-import * as React from 'react';
-import { Platform, StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
 
-const instructions = Platform.select({
-  ios: `Press Cmd+R to reload,\nCmd+D or shake for dev menu`,
-  android: `Double tap R on your keyboard to reload,\nShake or press menu button for dev menu`,
-});
+import _ from 'lodash'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.welcome}>Welcome to React Native!</Text>
-      <Text style={styles.instructions}>To get started, edit App.js</Text>
-      <Text style={styles.instructions}>{instructions}</Text>
-    </View>
-  );
+import {
+    View,
+    Text,
+    TextInput,
+    Button,
+    FlatList,
+    TouchableNativeFeedback,
+    StyleSheet,
+} from 'react-native';
+
+export default class App extends Component {
+
+    state = {
+        list: '',
+        userInput: '',
+        dict: [],
+    }
+
+    addToDo = (newTodo) => {
+        this.setState({ list: this.state.list.concat(this.state.userInput) })
+
+        let copyDict = this.state.dict;
+        copyDict[this.state.userInput] = false;
+
+        this.setState({ dict: copyDict });
+        console.log((this.state.dict));
+    }
+
+    updateUserInput = (text) => {
+        this.setState({ userInput: text })
+    }
+
+    toggleDone = (key) => {
+        let copyDict = this.state.dict;
+        if (this.state.dict[key]) {
+            copyDict[key] = false;
+        } else {
+            copyDict[key] = true;
+        }
+        this.setState({ dict: copyDict });
+        console.log((this.state.dict));
+    }
+
+    removeTodo = (key) => {
+        let copyDict = this.state.dict;
+        delete copyDict[key];
+        this.setState({ dict: copyDict });
+        console.log((this.state.dict));
+    }
+
+    renderItem = ({ item }) => {
+        const text = `${item}`;
+
+        if (this.state.dict[item]) {
+            return (
+                <View style={{ flex: 1, flexDirection: 'row' }}>
+                    <TouchableNativeFeedback onPress={() => this.toggleDone(item)}>
+                        <Text style={{ flex: 0.8, width: "100%", height: 48, backgroundColor: "white" }}>
+                            {text}
+                        </Text>
+
+                    </TouchableNativeFeedback>
+
+                    <TouchableNativeFeedback onPress={() => this.removeTodo(item)}>
+                        <Text style={{ flex: 0.2, width: "100%", height: 48, backgroundColor: "blue" }}>
+                            {'X'}
+                        </Text>
+
+                    </TouchableNativeFeedback>
+
+                </View>
+            );
+        } else {
+            return (
+                <View style={{ flex: 1, flexDirection: 'row' }}>
+                    <TouchableNativeFeedback onPress={() => this.toggleDone(item)}>
+                        <Text style={{ flex: 0.8, width: "100%", height: 48, backgroundColor: "red" }}>
+                            {text}
+                        </Text>
+
+                    </TouchableNativeFeedback>
+
+                    <TouchableNativeFeedback onPress={() => this.removeTodo(item)}>
+                        <Text style={{ flex: 0.2, width: "100%", height: 48, backgroundColor: "blue" }}>
+                            {'X'}
+                        </Text>
+
+                    </TouchableNativeFeedback>
+
+                </View>
+
+            );
+        }
+
+
+    };
+
+    render() {
+        return (
+            <View style={{ flex: 1 }} >
+                <View style={{ flex: 1, padding: 16 }}>
+                    <FlatList style={{ flex: 1, backgroundColor: "#aaa", borderColor: "gray", borderWidth: 1 }}
+                        renderItem={this.renderItem}
+                        data={_.keys(this.state.dict)}
+                        keyExtractor={item => `${item}`}
+
+                    />
+                </View>
+
+                <TextInput style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+                    onChangeText={text => this.updateUserInput(text)}
+                    ref={input => { this.textInput = input }}
+                />
+
+
+                <Button
+                    onPress={() => {
+                        this.addToDo();
+                        this.textInput.clear()
+                    }}
+                    title="Add todo"
+                />
+
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+    container: {
+        flex: 1,
+        paddingTop: 22
+    },
+    item: {
+        padding: 10,
+        fontSize: 10,
+        height: 44,
+    },
+})
