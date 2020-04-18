a6a33cea3d49816cd6b981671ea37f3fc1835de7
christianArby <christian.arby@gmail.com>
2020-04-18 13:57:13 +0200

    Polished first page

--- App.js
+++ App.js
@@ -9,14 +9,22 @@ import {
     Button,
     FlatList,
     TouchableNativeFeedback,
-    StyleSheet,
 } from 'react-native';
+
+import { Icon } from 'react-native-elements'
 
 export default class App extends Component {
 
     state = {
         userInput: '',
         dict: [],
+        data: [],
+        addItemExplanationVisible: true,
+        isLoading: true
+    }
+
+    setaddItemExplanationVisible = (boolean) => {
+        this.setState({ addItemExplanationVisible: boolean });
     }
 
     addToDo = (newTodo) => {
@@ -43,10 +51,130 @@ export default class App extends Component {
     }
 
     removeTodo = (key) => {
+        let dictSize = Object.keys(this.state.dict).length;
         let copyDict = this.state.dict;
         delete copyDict[key];
-        this.setState({ dict: copyDict });
+
+        
+        if (dictSize===1) {
+            this.setState({ dict: copyDict, userInput: '', addItemExplanationVisible:true });
+        } else {
+            this.setState({ dict: copyDict, userInput: ''});
+        }
         console.log((this.state.dict));
+    }
+
+    render() {
+        console.log(Object.keys(this.state.dict).length);
+        console.log('A');
+
+        if (Object.keys(this.state.dict).length === 0) {
+            if (this.state.addItemExplanationVisible) {
+                return (
+                    <View style={{ flex: 1, flexDirection: 'column', backgroundColor: 'red' }} >
+
+                        <FirstPage />
+
+                        <TouchableNativeFeedback onPress={() => {
+                            this.setaddItemExplanationVisible(false)
+                        }}>
+
+                            <View style={{ flex: 1, position: 'absolute', top: 0, left: 0, right: 0, bottom: 0, justifyContent: 'center', alignItems: 'center' }}>
+                                <View style={{ alignSelf: 'stretch', margin: 32, padding: 10, flexDirection: 'row', height: 50, borderColor: 'lightgray', borderWidth: 1, borderRadius: 25, backgroundColor: 'black', alignItems: 'center', justifyContent: 'center' }}>
+                                    <TouchableNativeFeedback
+                                        onPress={() => {
+                                            this.setaddItemExplanationVisible(false)
+                                        }}>
+
+                                        <View style={{ padding: 5 , flexDirection:'row'}} >
+                                            <Icon
+                                                name='add'
+                                                type='material'
+                                                color='white'
+                                                onPress={() => {
+                                                    this.setaddItemExplanationVisible(false)
+                                                }}
+                                            />
+
+                                            <Text style={{ fontSize: 16, color: 'white', paddingStart:8 }}>
+                                                {'Add item'}
+                                            </Text>
+                                        </View>
+
+
+
+
+                                    </TouchableNativeFeedback>
+
+
+
+                                </View>
+                            </View>
+
+
+                        </TouchableNativeFeedback>
+
+
+
+                    </View>
+                )
+            } else {
+                return (
+                    <View style={{ flex: 1, flexDirection: 'column', backgroundColor: 'red' }} >
+
+                        <FirstPage />
+
+                        <View style={{ flex: 1, position: 'absolute', top: 0, left: 0, right: 0, bottom: 0, justifyContent: 'center', alignItems: 'center' }}>
+                            <View style={{ alignSelf: 'stretch', margin: 32, padding: 10, flexDirection: 'row', height: 50, borderColor: 'lightgray', borderWidth: 1, borderRadius: 25, backgroundColor: 'white', alignItems: 'center' }}>
+                                <TextInput style={{ flexGrow: 1, height: 40, paddingStart: 10 }}
+                                    onChangeText={text => this.updateUserInput(text)}
+                                    ref={input => { this.textInput = input }}
+                                />
+                                <View style={{ padding: 5 }} >
+                                    <Icon
+                                        name='add'
+                                        type='material'
+                                        color='black'
+                                        onPress={() => {
+                                            this.addToDo();
+                                            this.textInput.clear()
+                                        }}
+                                    />
+                                </View>
+                            </View>
+                        </View>
+                    </View>
+                )
+            }
+
+        } else {
+            console.log('B');
+            return (
+
+                <View style={{ flex: 1 }} >
+                    <View style={{ flex: 1, padding: 16 }}>
+                        <FlatList style={{ flex: 1, backgroundColor: "#aaa", borderColor: "gray", borderWidth: 1 }}
+                            renderItem={this.renderItem}
+                            data={_.keys(this.state.dict)}
+                            keyExtractor={item => `${item}`}
+                        />
+                    </View>
+
+                    <TextInput style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+                        onChangeText={text => this.updateUserInput(text)}
+                        ref={input => { this.textInput = input }}
+                    />
+
+                    <Button
+                        onPress={() => {
+                            this.addToDo();
+                            this.textInput.clear()
+                        }}
+                        title="Add todo"
+                    />
+                </View>
+            );
+        }
     }
 
     renderItem = ({ item }) => {
@@ -92,31 +220,27 @@ export default class App extends Component {
             );
         }
     };
+}
 
-    render() {
-        return (
-            <View style={{ flex: 1 }} >
-                <View style={{ flex: 1, padding: 16 }}>
-                    <FlatList style={{ flex: 1, backgroundColor: "#aaa", borderColor: "gray", borderWidth: 1 }}
-                        renderItem={this.renderItem}
-                        data={_.keys(this.state.dict)}
-                        keyExtractor={item => `${item}`}
-                    />
-                </View>
+function FirstPage() {
+    return (
+        <View style={{ flex: 1 }}>
+            <View style={{ flex: 1, backgroundColor: 'white', justifyContent: 'center', alignItems: 'center' }}>
+                <Text style={{ fontSize: 26 }}>
+                    {'todo'}
+                </Text>
+                <Text style={{ fontSize: 16, marginTop: 8, color: 'gray' }}>
+                    {'Monday 25 Sep 2017'}
+                </Text>
 
-                <TextInput style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
-                    onChangeText={text => this.updateUserInput(text)}
-                    ref={input => { this.textInput = input }}
-                />
-
-                <Button
-                    onPress={() => {
-                        this.addToDo();
-                        this.textInput.clear()
-                    }}
-                    title="Add todo"
-                />
             </View>
-        );
-    }
+
+            <View style={{ flex: 1, backgroundColor: '#F6F6F6', justifyContent: 'center', alignItems: 'center' }}>
+                <Text style={{ fontSize: 16, marginTop: 8, color: 'gray', textAlign: 'center', lineHeight: 26 }}>
+                    {'What do you want to do today?\nStart adding items to your to-do list.'}
+                </Text>
+
+            </View>
+        </View>
+    )
 }
